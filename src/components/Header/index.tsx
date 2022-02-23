82e8cf0ecf05e30c47da022c70db780fd7149ed9
LucasDMS <lucasdelmondes14@live.com>
2022-02-23 20:45:17 -0300

    remove redirect to home in title header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,7 +6,7 @@ const Home: React.FC = () => {
     return (
         <Navbar bg="dark" variant="dark">
             <Container>
-                <Navbar.Brand href="/">Schedule</Navbar.Brand>
+                <Navbar.Brand>Schedule</Navbar.Brand>
                     <Nav className="me-auto">
                     <Nav.Item as={Link} className="nav-link" to ="/">Home</Nav.Item>
                     <Nav.Item as={Link} className="nav-link" to ="/tasks">Tasks</Nav.Item>
