b5375ee8754608596445a3a6e4fea6f8f6997b87
LucasDMS <lucasdelmondes14@live.com>
2022-02-23 20:43:32 -0300

    configure navbar navegation to use react-router-dom

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import {Navbar, Container, Nav} from 'react-bootstrap'
+import { Navbar, Container, Nav } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 
 const Home: React.FC = () => {
     return (
-        <Navbar bg="light" variant="light">
+        <Navbar bg="dark" variant="dark">
             <Container>
-                <Navbar.Brand href="/">Tasks</Navbar.Brand>
+                <Navbar.Brand href="/">Schedule</Navbar.Brand>
                     <Nav className="me-auto">
-                    <Nav.Link href="/">Home</Nav.Link>
-                    <Nav.Link href="/tasks">Tasks</Nav.Link>
+                    <Nav.Item as={Link} className="nav-link" to ="/">Home</Nav.Item>
+                    <Nav.Item as={Link} className="nav-link" to ="/tasks">Tasks</Nav.Item>
                 </Nav>
             </Container>
         </Navbar>
