32a0c7aad34f4681414bd5bf2718df7f5f9b48cc
LucasDMS <lucasdelmondes14@live.com>
2022-02-24 20:41:29 -0300

    making initial design and making data request in rest api

--- src/pages/Tasks/index.tsx
+++ src/pages/Tasks/index.tsx
@@ -1,7 +1,78 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Badge, Button, Table } from 'react-bootstrap'
+import api from '../../services/api'
+
+import moment from 'moment'
+
+interface ITasks {
+    id: number;
+    title: string;
+    description: string;
+    finished:boolean;
+    created_at: Date;
+    updated_at: Date;
+}
 
 const Home: React.FC = () => {
-    return <h1>Tasks page</h1>
+
+    const [ tasks, setTasks ] = useState<ITasks[]>([])
+
+    useEffect(() => {
+        loadTasks()
+    }, [])
+
+    async function loadTasks(){
+        const response = await api.get('/tasks')
+        console.log(response.data)
+        setTasks(response.data)
+    }
+
+    function formateDate(date: Date){
+        return moment(date).format('DD/MM/YYYY')
+    }
+
+    return (
+        <div className="container">
+            <br/>
+            <h1>Tasks</h1>
+            <br/>
+            <Table striped bordered hover responsive variant="dark" className="text-center">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Title</th>
+                        <th>Update date</th>
+                        <th>Status</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        tasks.map(task => (
+                            <tr key={ task.id }>
+                                <td>{ task.id }</td>
+                                <td>{ task.title }</td>
+                                <td>{ formateDate(task.updated_at) }</td>
+                                <td>
+                                    <Badge pill bg={ task.finished ? "success" : "warning" } text="dark">
+                                        {task.finished ? "Finished" : "Pending" }
+                                    </Badge>
+                                </td>
+                                <td>
+                                    <Button size="sm">Edit</Button>{' '}
+                                    <Button size="sm" variant="success">Finish</Button>{' '}
+                                    <Button size="sm" variant="info">View</Button>{' '}
+                                    <Button size="sm" variant="danger">Remove</Button>
+                                </td>
+                            </tr>
+                        ))
+                    }
+
+                    
+                </tbody>
+            </Table>
+        </div>
+    );
 }
 
 export default Home;
