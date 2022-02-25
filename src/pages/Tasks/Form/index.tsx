f894aa5624adc987d54a72f17eb540e728125489
LucasDMS <lucasdelmondes14@live.com>
2022-02-24 21:03:53 -0300

    starting form tasks

---
+++ src/pages/Tasks/Form/index.tsx
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from "react";
+import { Badge, Button, Table } from 'react-bootstrap'
+
+import moment from 'moment'
+
+import './index.css'
+
+interface ITasks {
+    id: number;
+    title: string;
+    description: string;
+    finished:boolean;
+    created_at: Date;
+    updated_at: Date;
+}
+
+const Form: React.FC = () => {
+
+    return (
+        <div className="container">
+            <br/>
+            <div className="task-header d-flex">
+                <h1>Tasks</h1>
+                <Button variant="dark" size="sm">Back</Button>
+            </div>
+            
+            
+        </div>
+    );
+}
+
+export default Form;
