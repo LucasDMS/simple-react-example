2fc0255b7f80508c6f638555a7a3a03773f3d414
LucasDMS <lucasdelmondes14@live.com>
2022-03-07 19:50:03 -0300

    making basic form design

--- src/pages/Tasks/Form/index.tsx
+++ src/pages/Tasks/Form/index.tsx
@@ -23,20 +23,14 @@ const TasksForm: React.FC = () => {
             </div>
             <div className="row container">
                 <Form>
-                    <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
-                        <Form.Text className="text-muted">
-                        We'll never share your email with anyone else.
-                        </Form.Text>
+                    <Form.Group>
+                        <Form.Label>Title</Form.Label>
+                        <Form.Control type="text" placeholder="Enter title" />
                     </Form.Group>
 
-                    <Form.Group className="mb-3" controlId="formBasicPassword">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                        <Form.Check type="checkbox" label="Check me out" />
+                    <Form.Group>
+                        <Form.Label>Description</Form.Label>
+                        <Form.Control as="textarea" rows={3} />
                     </Form.Group>
                     <Button variant="primary" type="submit">
                         Submit
